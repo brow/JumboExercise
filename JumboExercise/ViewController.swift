96266894b106a21728e3a644867d4aade2a83d24
Tom Brow <tom@tombrow.com>
2019-12-20 12:13:55 -0800

    don't use Storyboards

--- JumboExercise/ViewController.swift
+++ JumboExercise/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override init(
+        nibName nibNameOrNil: String?,
+        bundle nibBundleOrNil: Bundle?)
+    {
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .systemBackground
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
-
